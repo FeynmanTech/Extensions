a2ddf314a7a489b70169bd0321f4a17f73db24de
FeynmanTech <astro.link.75@gmail.com>
2015-03-04 19:43:59 -0800

    Add actual content

--- TinyMCE.js
+++ TinyMCE.js
@@ -1,1 +1,35 @@
+// ==UserScript==
 
+// @name          WYSIWYG Enhancement
+
+// @namespace     http://www.powdertoy.co.uk
+
+// @description   Replaces default TPT WYSIWYG with the TinyMCE editor
+
+// @include       http://powdertoy.co.uk/*
+
+// @require       http://tinymce.cachefly.net/4.1/tinymce.min.js
+
+// @updateURL     http://raw.githubusercontent.com/FeynmanTech/Extensions/master/TinyMCE.js
+
+// @version       0.5
+
+// ==/UserScript==
+
+/*
+var head = document.getElementsByTagName("head");
+head[1].innerHTML = head[1].innerHTML + '\n<script src="//tinymce.cachefly.net/4.1/tinymce.min.js"></script>';
+*/
+
+tinyMCE.baseURL = "http://files.brilliant-minds.tk/~feynman";
+
+tinymce.init({
+    skin: "lightgraynofonts",
+	selector: "textarea",
+	plugins: [
+		"advlist autolink lists link image charmap print preview anchor",
+		"searchreplace visualblocks code fullscreen",
+		"insertdatetime media table contextmenu paste"
+	],
+	toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | save"
+});
